813e42e1aa7d36309ca45bf7e4429c66671d2b22
SZN13YT <szabo.nandor.istvan@gmail.com>
2025-05-22 23:03:00 +0200

    Update main.py

--- dice_range_expression/main.py
+++ dice_range_expression/main.py
@@ -1,4 +1,6 @@
 with open('./input.txt', 'r') as f:
-  input = f.read()
+  input = [[int(i) for i in _.strip().split()] for _ in f.readlines()]
+dices = {"d20": 20, "d10": 10, "d8": 8, "d6": 6, "d4": 4, "d3": 3, "d2": 2}
+for i in input:
+  for j in i:
 
-print(input)
